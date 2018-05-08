f3c4ca9b4bf34278c866637341b850220cb1182d
suthan somu <suthan.somu@gmail.com>
2018-05-08 07:49:08 +0530

    test

--- get_date_from_git_log.sh
+++ get_date_from_git_log.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+month_selected=$1
+year_selected=$2
+
 reporoot=`pwd`
 cd ${reporoot}
 last_commit_date=`git log | head -n 3 | grep "^Date:"`
@@ -9,13 +12,15 @@ last_commit_month=`echo ${last_commit_date} | tr -s ' ' | cut -d ' ' -f 3`
 echo "$last_commit_year"
 echo "$last_commit_month"
 
-case ${last_commit_month} in
+convert_month()
+{
+case $1 in
   Jan) month_num=01 ;;
   Feb) month_num=02 ;;
   Mar) month_num=03 ;;
   Apr) month_num=04 ;;
   May) month_num=05 ;;
-  Jun) month_num=06 ;;
+  Jun|June) month_num=06 ;;
   Jul) month_num=07 ;;
   Aug) month_num=08 ;;
   Sep) month_num=09 ;;
@@ -25,7 +30,18 @@ case ${last_commit_month} in
 esac
 
 echo $month_num
+}
 
+convert_month ${last_commit_month}
 year_n_month=${last_commit_year}${month_num}
 
 echo "${year_n_month}"
+
+convert_month ${month_selected}
+selected_year_n_month=${year_selected}${month_num}
+echo "${selected_year_n_month}"
+
+if [[ "${selected_year_n_month}" -le "${year_n_month}" ]]; then
+	echo "Branch is older"
+	
+fi
