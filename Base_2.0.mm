7caf02daa294480a2bdb25064eb6baddc3257cef
suthan somu <suthan.somu@gmail.com>
2018-06-03 14:14:33 +0530

    add new file

---
+++ Base_2.0.mm
@@ -0,0 +1,14 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1527954156182" ID="ID_381267405" MODIFIED="1527954170338" TEXT="Base 2.0">
+<node CREATED="1527954176551" HGAP="18" ID="ID_155871824" MODIFIED="1527954191852" POSITION="right" TEXT="Init Project" VSHIFT="-102">
+<node CREATED="1527954316085" HGAP="28" ID="ID_867151107" MODIFIED="1527954390522" TEXT="Runs on Rundeck server" VSHIFT="6"/>
+<node CREATED="1527954382012" HGAP="35" ID="ID_158788469" MODIFIED="1527954472029" TEXT="Creates project root for preparing deployment artifacts in &quot;/apps/codedist/deploy/rundeck/deployhome&quot; " VSHIFT="-58"/>
+<node CREATED="1527954488845" HGAP="22" ID="ID_1675652445" MODIFIED="1527954524455" TEXT="Create project root /apps/codedist/deploy/rundeck/deployhome/&lt;project-root&gt;" VSHIFT="1"/>
+<node CREATED="1527954541846" ID="ID_647329375" MODIFIED="1527954994583" TEXT="Create symlink of rundeck job log under /fdc/log/rundeck/buildlink with name as associated jenkins job">
+<icon BUILTIN="yes"/>
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+</map>
